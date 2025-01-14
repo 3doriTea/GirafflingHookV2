ee541b8aa511b1601fd0220beeda2cc02e12c1e4
GreenTea-Mi <sorasora3program@gmail.com>
2025-01-15 06:39:52 +0900

    ✨イベント処理追加中

--- GirafflingHookV2/MiniEngine/Event.cpp
+++ GirafflingHookV2/MiniEngine/Event.cpp
@@ -1,1 +1,34 @@
 #include "Event.h"
+
+Event::EventElement::EventElement(
+	const EventHandle& _handle,
+	const float& _timeMax,
+	const EventCallback& _callback) :
+	HANDLE{ _handle },
+	TIME_MAX{ _timeMax },
+	callback{ _callback },
+	timeLeft{ _timeMax }
+{
+}
+
+void Event::EventElement::Invoke() const
+{
+	callback();
+}
+
+EventHandle Event::SetInterval(const EventCallback& callback, const float& seconds)
+{
+	EventElement element{ handleCounter, seconds, callback };
+	events.insert({ handleCounter, element });
+	handleCounter++;
+	return element.HANDLE;
+}
+
+EventHandle Event::SetTimeout(const EventCallback& callback, const float& seconds)
+{
+	return EventHandle();
+}
+
+void Event::Delete(const EventHandle& handle)
+{
+}
--- GirafflingHookV2/MiniEngine/Event.h
+++ GirafflingHookV2/MiniEngine/Event.h
@@ -1,28 +1,44 @@
 #pragma once
 #include <functional>
+#include <map>
 
 using EventHandle = int;
+using EventCallback = std::function<void()>;
 
 class Event
 {
+	enum struct EventType : char
+	{
+		Interval,
+		Timeout,
+	};
 	struct EventElement
 	{
-		void Invoke();
+		EventElement(
+			const EventHandle& _handle,
+			const float& _timeMax,
+			const EventCallback& _callback);
 
+		void Invoke() const;
 
-		std::function<void()> callback;
-		float milliseconds;
+		EventCallback callback;
+		const float TIME_MAX;
+		const EventHandle HANDLE;
+		const EventType
+		float timeLeft;
 	};
 
 public:
 	static EventHandle SetInterval(
-		const std::function<void()>& callback,
-		const float& milliseconds);
+		const EventCallback& callback,
+		const float& seconds);
 	static EventHandle SetTimeout(
-		const std::function<void()>& callback,
-		const float& milliseconds);
+		const EventCallback& callback,
+		const float& seconds);
+	static void Delete(const EventHandle& handle);
 
 private:
-
+	static std::map<EventHandle, EventElement> events;
+	static EventHandle handleCounter;
 };
 
