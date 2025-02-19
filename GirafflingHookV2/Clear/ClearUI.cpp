0880a9a68127208f933f6736e0e76f5f507d1160
啓大 齋藤 <240004@jc-21.jp>
2025-02-19 23:35:21 +0900

    ✨️ ClearUI

--- GirafflingHookV2/Clear/ClearUI.cpp
+++ GirafflingHookV2/Clear/ClearUI.cpp
@@ -21,14 +21,21 @@ void Clear::ClearUI::Init()
 {
 	for (int i = 0; i < 10; i++)
 	{
-		std::string path = "Assets/UI/numModel_"; // 分離してね
-		path.push_back('0' + i); // intの数値をcharにキャストすると、文字コードになるのである！
+		std::string path = "Assets/UI/num/numModel_"; // 分離してね
+		//path.push_back(static_cast<char>(i)) ← 文字コードで突っ込んでる（しかも今回の場合は制御文字！）
+		path.push_back('0' + i); // ← 考えたやつ頭いい
+		path += ".mv1";
 		hNumModel.push_back(MV1LoadModel(path.c_str()));
+		assert(hNumModel[i] != -1);
 	}
 }
 
 void Clear::ClearUI::Draw() const
 {
+	for (int i = 0; i < 10; i++)
+	{
+		MV1DrawModel(hNumModel[i]);
+	}
 }
 
 void Clear::ClearUI::End()
